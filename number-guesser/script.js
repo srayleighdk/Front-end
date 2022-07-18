6a7f4bfed8cb44385c9e3d4ed63e1cdbbc3ed39d
Silvers Rayleigh <srayleighdk@gmail.com>
2022-07-17 20:13:59 -0400

    fix and finish number-guesser

---
+++ number-guesser/script.js
@@ -0,0 +1,47 @@
+// let humanScore = 0;
+// let computerScore = 0;
+// let currentRoundNumber = 1;
+
+// const generateTarget = () => {
+//     return Math.floor(Math.random() * 10);
+// }
+
+// const compareGuesses = (humanGuess, computerGuess, targetNumber) => {
+//     const humanDifference = Math.abs(targetNumber - humanGuess);
+//     const computerDifference = Math.abs(targetNumber - computerGuess)
+//     return humanDifference <= computerDifference;
+// }
+
+// const updateScore = winner => {
+//     if (winner === 'human') {
+//         humanScore++;
+//     } else if (winner === 'computer') {
+//         computerScore++
+//     }
+// }
+
+// const advanceRound = () => currentRoundNumber++;
+
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
+}
+
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+  const humanDifference = Math.abs(targetGuess - humanGuess)
+  const computerDifference = Math.abs(targetGuess - computerGuess)
+  return humanDifference <= computerDifference;
+}
+
+const updateScore = winner => {
+  if (winner === 'human') {
+    humanScore++;
+  } else if (winner === 'computer') {
+    computerScore++;
+  }
+}
+
+const advanceRound = () => currentRoundNumber++;
