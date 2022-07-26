98b9781f4bda56c1f808c5877a9631e83b995012
Silvers Rayleigh <srayleighdk@gmail.com>
2022-07-25 21:27:59 -0400

    Add portfolio website

--- number-guesser/game.js
+++ number-guesser/game.js
@@ -1,99 +1,3 @@
-// let target;
-
-// const humanGuessInput = document.getElementById('human-guess');
-
-// const roundNumberDisplay = document.getElementById('round-number');
-
-// const computerGuessDisplay = document.getElementById('computer-guess');
-
-// const humanScoreDisplay = document.getElementById('human-score');
-
-// const computerScoreDisplay = document.getElementById('computer-score');
-
-// const targetNumberDisplay = document.getElementById('target-number');
-
-// const computerWinsDisplay = document.getElementById('computer-wins');
-
-// const guessButton = document.getElementById('guess');
-// const nextRoundButton = document.getElementById('next-round');
-
-
-// guessButton.addEventListener('click', () => {
-//     target = generateTarget();
-
-//     const currentHumanGuess = humanGuessInput.value;
-//     const computerGuess = Math.floor(Math.random() * 10);
-
-//     computerGuessDisplay.innerText = computerGuess;
-//     targetNumberDisplay.inerText = target;
-
-//     const humanisWinner = compareGuesses(currentHumanGuess, computerGuess, target);
-//     const winner = humanisWinner ? 'human' : 'computer';
-
-//     updateScore(winner);
-
-//     if (humanisWinner) {
-//         guessButton.innerText = "You Win!!!";
-//         guessButton.classList.toggle('winner-text');
-//     } else {
-//         computerWinsDisplay.innerText = "Computer Win!!!";
-//     }
-
-//     humanScoreDisplay.innerText = humanScore;
-//     computerScoreDisplay.innerText = computerScore;
-
-//     guessButton.setAttribute('disabled', true);
-//     nextRoundButton.removeAttribute('disabled');
-// });
-
-// nextRoundButton.addEventListener('click', () => {
-//     advanceRound();
-
-//     roundNumberDisplay.innerText = currentRoundNumber;
-
-//     nextRoundButton.setAttribute('disabled', true);
-//     guessButton.removeAttribute('disabled');
-
-//     targetNumberDisplay.innerText = '?';
-//     guessButton.innerText = 'Make a guess';
-//     humanGuessInput.value = '';
-//     computerGuessDisplay.innerText = '?';
-//     computerWinsDisplay.innerText = '';
-//     guessButton.classList.remove('winning-text');
-// });
-
-// const addButton = document.getElementById('add');
-// const subtractButton = document.getElementById('subtract');
-
-// addButton.addEventListener('click', () => {
-//     humanGuessInput.value = +humanGuessInput.value + 1;
-//     handleValueChange(humanGuessInput.value);
-// });
-
-// subtractButton.addEventListener('click', () => {
-//     humanGuessInput.value = +humanGuessInput.value - 1;
-//     handleValueChange(humanGuessInput.value);
-// });
-
-
-// const handleValueChange = value => {
-//     if (value > 0 && value <=9) {
-//         subtractButton.removeAttribute('disabled');
-//         addButton.removeAttribute('disabled');
-//     } else if (value > 9 ) {
-//         addButton.setAttribute('disable', true);
-//     } else if (value <= 0 ) {
-//         subtractButton.setAttribute('disable', true);
-//     }
-// }
-
-// humanGuessInput.addEventListener('input', function(e) {
-//     handleValueChange(e.target.value);
-// })
-
-
-
-
 let target;
 
 const humanGuessInput = document.getElementById('human-guess');
@@ -101,95 +5,92 @@ const humanGuessInput = document.getElementById('human-guess');
 const roundNumberDisplay = document.getElementById('round-number');
 
 const computerGuessDisplay = document.getElementById('computer-guess');
+
 const humanScoreDisplay = document.getElementById('human-score');
+
 const computerScoreDisplay = document.getElementById('computer-score');
+
 const targetNumberDisplay = document.getElementById('target-number');
+
 const computerWinsDisplay = document.getElementById('computer-wins');
 
 const guessButton = document.getElementById('guess');
-const nextRoundButton = document.getElementById('next-round')
+const nextRoundButton = document.getElementById('next-round');
+
 
 guessButton.addEventListener('click', () => {
-  // Generate the target value
-  target = generateTarget();
-  // Retrieve the player's guess
-  const currentHumanGuess = humanGuessInput.value;
-  // Make a random 'computer guess'
-  const computerGuess = Math.floor(Math.random() * 10);
+    target = generateTarget();
 
-  // Display the computer guess and the target
-  computerGuessDisplay.innerText = computerGuess;
-  targetNumberDisplay.innerText = target;
-  
-  // Determine if the human or computer wins:
-  const humanIsWinner = compareGuesses(currentHumanGuess, computerGuess, target)
-  const winner = humanIsWinner ? 'human' : 'computer'
+    const currentHumanGuess = humanGuessInput.value;
+    const computerGuess = Math.floor(Math.random() * 10);
 
-  // Update the correct score:
-  updateScore(winner);
+    computerGuessDisplay.innerText = computerGuess;
+    targetNumberDisplay.inerText = target;
 
-  // Display the winner
-  if (humanIsWinner) {
-    guessButton.innerText = 'You Win!!!!!';
-    guessButton.classList.toggle('winning-text')
-  } else {
-    computerWinsDisplay.innerText = 'Computer Wins!!!';
-  }
+    const humanisWinner = compareGuesses(currentHumanGuess, computerGuess, target);
+    const winner = humanisWinner ? 'human' : 'computer';
 
-  // winnerDisplay.innerText = humanIsWinner ? 'You win!' : 'Computer wins!';
+    updateScore(winner);
 
-  // Display the current scores:
-  humanScoreDisplay.innerText = humanScore;
-  computerScoreDisplay.innerText = computerScore;
-  
-  // Set the correct disabled state for the buttons
-  guessButton.setAttribute('disabled', true)
-  nextRoundButton.removeAttribute('disabled');
+    if (humanisWinner) {
+        guessButton.innerText = "You Win!!!";
+        guessButton.classList.toggle('winner-text');
+    } else {
+        computerWinsDisplay.innerText = "Computer Win!!!";
+    }
+
+    humanScoreDisplay.innerText = humanScore;
+    computerScoreDisplay.innerText = computerScore;
+
+    guessButton.setAttribute('disabled', true);
+    nextRoundButton.removeAttribute('disabled');
 });
 
 nextRoundButton.addEventListener('click', () => {
-  // Increase the round number
-  advanceRound();
-  // Display the new round number
-  roundNumberDisplay.innerText = currentRoundNumber;
+    advanceRound();
 
-  // Set the correct disabled state for the buttons
-  nextRoundButton.setAttribute('disabled', true);
-  guessButton.removeAttribute('disabled');
+    roundNumberDisplay.innerText = currentRoundNumber;
 
-  // Reset the guess input box and the target number display:
-  targetNumberDisplay.innerText = '?';
-  guessButton.innerText = 'Make a Guess';
-  humanGuessInput.value = '';
-  computerGuessDisplay.innerText = '?';
-  computerWinsDisplay.innerText = '';
-  guessButton.classList.remove('winning-text');
+    nextRoundButton.setAttribute('disabled', true);
+    guessButton.removeAttribute('disabled');
+
+    targetNumberDisplay.innerText = '?';
+    guessButton.innerText = 'Make a guess';
+    humanGuessInput.value = '';
+    computerGuessDisplay.innerText = '?';
+    computerWinsDisplay.innerText = '';
+    guessButton.classList.remove('winning-text');
 });
 
 const addButton = document.getElementById('add');
 const subtractButton = document.getElementById('subtract');
 
 addButton.addEventListener('click', () => {
-  humanGuessInput.value = +humanGuessInput.value + 1;
-  handleValueChange(humanGuessInput.value);
+    humanGuessInput.value = +humanGuessInput.value + 1;
+    handleValueChange(humanGuessInput.value);
 });
 
 subtractButton.addEventListener('click', () => {
-  humanGuessInput.value = +humanGuessInput.value - 1;
-  handleValueChange(humanGuessInput.value);
+    humanGuessInput.value = +humanGuessInput.value - 1;
+    handleValueChange(humanGuessInput.value);
 });
 
+
 const handleValueChange = value => {
-  if (value > 0 && value <= 9) {
-    subtractButton.removeAttribute('disabled');
-    addButton.removeAttribute('disabled');
-  } else if (value > 9) {
-    addButton.setAttribute('disabled', true);
-  } else if (value <= 0) {
-    subtractButton.setAttribute('disabled', true);
-  }
+    if (value > 0 && value <=9) {
+        subtractButton.removeAttribute('disabled');
+        addButton.removeAttribute('disabled');
+    } else if (value > 9 ) {
+        addButton.setAttribute('disable', true);
+    } else if (value <= 0 ) {
+        subtractButton.setAttribute('disable', true);
+    }
 }
 
 humanGuessInput.addEventListener('input', function(e) {
-  handleValueChange(e.target.value);
-});
+    handleValueChange(e.target.value);
+})
+
+
+
+
